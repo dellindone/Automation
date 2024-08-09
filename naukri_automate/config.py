4b2aae6f4a06ec9114d5c8fe185b73d618dcced2
dellindone <44501353+dellindone@users.noreply.github.com>
2024-08-09 20:46:39 +0530

    Update config.py
    config comments

--- naukri_automate/config.py
+++ naukri_automate/config.py
@@ -1,3 +1,10 @@
-account_name=''
+account_name=''   
 password=''
 app_password=""
+
+
+"""
+  Account name is email id of naukri and gmail
+  password is for naukri
+  app password is for sending mail
+"""
